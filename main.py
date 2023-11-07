43a5806d2909a77c1cfaa3e2adaee98b228e2230
Japamaja <Jacksontrenerry01@gmail.com>
2023-11-07 21:19:52 +1000

    Seperates functions for better code practices

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# 'Runs the code' that gives the output and writes it to the output file
+def output(size: str, name: str, frameHeight: int, frameWidth: int, frameSize: int, framesPerRow: int, frameCount: int):
+    # Makes output file
+    outputFileName = 'output\\' + name.split(".", 1)[0].strip('.') + ".atlas"
+    outputFile = open(outputFileName, "w")
+    # Starts the output process
+    outputFile.write(name + "\nsize: " + size + "\nformat: RGBA8888\nfilter: Nearest,Nearest\nrepeat: none")
+    f = -1
+    for i in range(0, frameCount):
+        if ((i % framesPerRow) == 0):
+            f += 1
+        x = frameWidth * (i % framesPerRow)
+        y = frameHeight * (f)
+        outputFile.write("\ndefault\n  rotate: false\n  xy: " + str(x) + "," + str(y) + "\n  size: " + frameSize + "\n  orig: " + frameSize +"\n  offset: 0,0\n  index: " + str(i))
+    return
+
 def main():
     # Get the name of the image
     name = input("The filename of the image (including the extension): ")
@@ -17,18 +33,7 @@ def main():
     imageWidth = framesPerRow * frameWidth
     imageHeight = framesPerColumn * frameHeight
     size = str(imageWidth) + ", " + str(imageHeight)
-    # Makes output file
-    outputFileName = 'output\\' + name.split(".", 1)[0].strip('.') + ".atlas"
-    outputFile = open(outputFileName, "w")
-    # Starts the output process
-    outputFile.write(name + "\nsize: " + size + "\nformat: RGBA8888\nfilter: Nearest,Nearest\nrepeat: none")
-    f = -1
-    for i in range(0, frameCount):
-        if ((i % framesPerRow) == 0):
-            f += 1
-        x = frameWidth * (i % framesPerRow)
-        y = frameHeight * (f)
-        outputFile.write("\ndefault\n  rotate: false\n  xy: " + str(x) + "," + str(y) + "\n  size: " + frameSize + "\n  orig: " + frameSize +"\n  offset: 0,0\n  index: " + str(i))
+    output(size, name, frameHeight, frameWidth, frameSize, framesPerRow, frameCount)
     return
 
 if __name__ == "__main__":
